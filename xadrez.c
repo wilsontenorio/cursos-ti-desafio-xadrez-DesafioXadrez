c6a6e6030effb80cbb078629399eafa39bb65ffc
wilsontenorio <nosliwilson@hotmail.com>
2025-06-08 18:38:22 -0300

    Update xadrez.c
    Update Nivel Aventureiro
    Movimento do Cavalo

--- xadrez.c
+++ xadrez.c
@@ -12,10 +12,14 @@ int main() {
     printf("1. Torre\n");
     printf("2. Bispo\n");
     printf("3. Rainha\n");
+    printf("4. Cavalo\n");
     scanf("%d", &opcaomenu);
     switch (opcaomenu) // Inicia o switch para verificar a opção escolhida pelo usuário
-
     {
+    case 0: // Caso o usuário escolha 0, encerra o programa
+        printf("Você escolheu não mover nenhuma peça. Até logo!\n");
+        return 0; // Retorna 0 para indicar que o programa foi executado com sucesso
+        break;
     case 1: // Caso o usuário escolha a Torre solicita a direção e a quantidade de casas que deseja mover
         printf("Você escolheu mover a Torre.\n");
         printf("A Torre se move em linha reta, horizontal ou verticalmente.\n");
@@ -179,13 +183,115 @@ int main() {
                         break;
                     default: printf("Você escolheu uma direção inválida.\n"); break;
                 }
-    case 0:
-        printf("Você escolheu sair do jogo. Até logo!\n");
+    case 4: // Caso o usuário escolha o Cavalo solicita a direção e a quantidade de casas que deseja mover
+        printf("Você escolheu mover o Cavalo.\n");
+        printf("O Cavalo se move em forma de L, duas casas em uma direção e uma casa em outra.\n");
+        printf("Em qual direção deseja mover o Cavalo?\n");
+        printf("0. Nenhuma, Sair\n");
+        printf("1. Duas casas para Cima e uma para a Esquerda\n");
+        printf("2. Duas casas para Cima e uma para a Direita\n");
+        printf("3. Duas casas para Baixo e uma para a Esquerda\n");
+        printf("4. Duas casas para Baixo e uma para a Direita\n");
+        printf("5. Uma casa para Cima e duas para a Esquerda\n");
+        printf("6. Uma casa para Cima e duas para a Direita\n");
+        printf("7. Uma casa para Baixo e duas para a Esquerda\n");
+        printf("8. Uma casa para Baixo e duas para a Direita\n");
+        int direcaoCavalo;
+        int mov = 1; // Variável para controlar a movimentação do Cavalo
+        scanf("%d", &direcaoCavalo);
+            if (direcaoCavalo == 0) {
+                printf("Você escolheu não mover o Cavalo. Até logo!\n");
+            } else {
+                switch (direcaoCavalo) { // Recebe a direção escolhida pelo usuário e exibe a movimentação do Cavalo.
+                    case 1:  //Duas casas para Cima e uma para a Esquerda
+                        for (int i = 1; i < 2; i++) { // Controla as duas casas em uma direção
+                            int mov = 1;
+                            while (mov <= 2) { // Controla a casa adicional em outra direção
+                                printf("Movendo o Cavalo para a Cima.\n");
+                                mov++;
+                            }
+                            printf("Movendo o Cavalo para Esquerda.\n");
+                        }
+                    break;
+                    case 2: //Duas casas para Cima e uma para a Direita
+                        for (int i = 1; i < 2; i++) { // Controla as duas casas em uma direção
+                            int mov = 1;
+                            while (mov <= 2) { // Controla a casa adicional em outra direção
+                                printf("Movendo o Cavalo para a Cima.\n");
+                                mov++;
+                            }
+                            printf("Movendo o Cavalo para Direita.\n");
+                        }
+                        break;
+                    case 3: //Duas casas para Baixo e uma para a Esquerda
+                        for (int i = 1; i < 2; i++) { // Controla as duas casas em uma direção
+                            int mov = 1;
+                            while (mov <= 2) { // Controla a casa adicional em outra direção
+                                printf("Movendo o Cavalo para Baixo.\n");
+                                mov++;
+                            }
+                            printf("Movendo o Cavalo para Esquerda.\n");
+                        }
+                    break;
+                    case 4: //Duas casas para Baixo e uma para a Direita
+                        for (int i = 1; i < 2; i++) { // Controla as duas casas em uma direção
+                            int mov = 1;
+                            while (mov <= 2) { // Controla a casa adicional em outra direção
+                                printf("Movendo o Cavalo para a Baixo.\n");
+                                mov++;
+                            }
+                            printf("Movendo o Cavalo para Direita.\n");
+                        }
+                        break;
+                    case 5: //Uma casa para Cima e duas para a Esquerda
+                        for (int i = 1; i < 2; i++) { // Controla as duas casas em uma direção
+                            printf("Movendo o Cavalo para Cima.\n");
+                            int mov = 1;
+                            while (mov <= 2) { // Controla a casa adicional em outra direção
+                                printf("Movendo o Cavalo para Esquerda.\n");
+                                mov++;
+                            }
+                        }
+                        break;
+                    case 6: //Uma casa para Cima e duas para a Direita
+                        for (int i = 1; i < 2; i++) { // Controla as duas casas em uma direção
+                            printf("Movendo o Cavalo para Cima.\n");
+                            int mov = 1;
+                            while (mov <= 2) { // Controla a casa adicional em outra direção
+                                printf("Movendo o Cavalo para a Direita.\n");
+                                mov++;
+                            }
+                        }
+                        break;
+                    case 7: //Uma casa para Baixo e duas para a Esquerda
+                        for (int i = 1; i < 2; i++) { // Controla as duas casas em uma direção
+                            printf("Movendo o Cavalo para Baixo.\n");
+                            int mov = 1;
+                            while (mov <= 2) { // Controla a casa adicional em outra direção
+                                printf("Movendo o Cavalo para a Esquerda.\n");
+                                mov++;
+                            }
+                        }
+                        break;
+                    case 8: //Uma casa para Baixo e duas para a Direita
+                        for (int i = 1; i < 2; i++) { // Controla as duas casas em uma direção
+                            printf("Movendo o Cavalo para Baixo.\n");
+                            int mov = 1;
+                            while (mov <= 2) { // Controla a casa adicional em outra direção
+                                printf("Movendo o Cavalo para a Direita.\n");
+                                mov++;
+                            }
+                        }
+                        break;
+                    default:
+                        printf("Você escolheu uma direção inválida.\n");
+                        break;
+                }
+                }
+            }
         break;
     default:
         printf("Opção inválida. Por favor, escolha uma opção válida.\n");
         break;
     }
-} 
-    return 0; // Retorna 0 para indicar que o programa foi executado com sucesso
 }
